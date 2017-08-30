1f9cab1615139e907b6f4ccccb6e91cac2b2e76c
Plx0wn <lucas.rouss@gmail.com>
2017-08-30 19:31:03 +0200

    Create Utils.java

---
+++ src/fr.plx0wn/Utils.java
@@ -0,0 +1,12 @@
+package fr.plx0wn;
+
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Utils {
+	
+	public static String ColoredString(String str){
+		return ChatColor.translateAlternateColorCodes('&', str);
+	}
+
+}
