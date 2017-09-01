6bd81ce72ed433cf9272c62c582b39c480b77235
Plx0wn <lucas.rouss@gmail.com>
2017-09-01 13:19:31 +0200

    Update Mobs.java

--- src/fr.plx0wn/Rewards/Mobs.java
+++ src/fr.plx0wn/Rewards/Mobs.java
@@ -3,6 +3,7 @@ package fr.plx0wn.Rewards;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventException;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
@@ -16,19 +17,24 @@ public class Mobs implements Listener {
 	public static FileConfiguration msgconf = MobsReward.msgconf;
 
 	@SuppressWarnings("deprecation")
-	public static void ifTypeIs(EntityType killed, EntityType type, String name, Player player, double reward) {
-		if (killed.equals(type)) {
-			if (config.getBoolean("enabled." + name)) {
-				if (reward < 0) {
-					MobsReward.economy.withdrawPlayer(player.getName(), -reward).transactionSuccess();
-					player.sendMessage(
-							Utils.ColoredString(msgconf.getString("mobs.loose." + name)).replace("<reward>", "" + reward));
-				} else {
-					MobsReward.economy.depositPlayer(player.getName(), reward).transactionSuccess();
-					player.sendMessage(
-							Utils.ColoredString(msgconf.getString("mobs.earn." + name)).replace("<reward>", "" + reward));
+	public static void ifTypeIs(EntityType killed, String type, String name, Player player, double reward) {
+		try {
+			if (killed.equals(EntityType.fromName(type))) {
+				if (config.getBoolean("enabled." + name)) {
+					if (player.hasPermission("rewards.mobs." + name) || player.hasPermission("rewards.mobs.*")) {
+						if (reward < 0) {
+							MobsReward.economy.withdrawPlayer(player.getName(), -reward).transactionSuccess();
+							player.sendMessage(Utils.ColoredString(msgconf.getString("mobs.loose." + name))
+									.replace("<reward>", "" + reward));
+						} else {
+							MobsReward.economy.depositPlayer(player.getName(), reward).transactionSuccess();
+							player.sendMessage(Utils.ColoredString(msgconf.getString("mobs.earn." + name))
+									.replace("<reward>", "" + reward));
+						}
+					}
 				}
 			}
+		} catch (Exception e) {
 		}
 	}
 
@@ -36,47 +42,65 @@ public class Mobs implements Listener {
 	public void onEntityKill(EntityDeathEvent e) {
 
 		double blaze = config.getDouble("mobs.blaze");
-		double cave_spider = config.getDouble("mobs.cave_spider");
+		double caveSpider = config.getDouble("mobs.cave_spider");
 		double creeper = config.getDouble("mobs.creeper");
+		double elderGuardian = config.getDouble("mobs.elder_guardian");
 		double enderDragon = config.getDouble("mobs.ender_dragon");
 		double enderman = config.getDouble("mobs.enderman");
 		double endermite = config.getDouble("mobs.endermite");
+		double evoker = config.getDouble("mobs.evoker");
 		double ghast = config.getDouble("mobs.ghast");
 		double giant = config.getDouble("mobs.giant");
 		double guardian = config.getDouble("mobs.guardian");
+		double husk = config.getDouble("mobs.husk");
+		double illusioner = config.getDouble("mobs.illusioner");
 		double magmaCube = config.getDouble("mobs.magma_cube");
 		double pigZombie = config.getDouble("mobs.pig_zombie");
+		double shulker = config.getDouble("mobs.shulker");
 		double silverfish = config.getDouble("mobs.silverfish");
 		double skeleton = config.getDouble("mobs.skeleton");
 		double slime = config.getDouble("mobs.slime");
 		double spider = config.getDouble("mobs.spider");
+		double stray = config.getDouble("mobs.stray");
+		double vex = config.getDouble("mobs.vex");
+		double vindicator = config.getDouble("mobs.vindicator");
 		double witch = config.getDouble("mobs.witch");
 		double wither = config.getDouble("mobs.wither");
 		double witherSkeleton = config.getDouble("mobs.wither_skeleton");
 		double zombie = config.getDouble("mobs.zombie");
+		double zombieVillager = config.getDouble("mobs.zombie_villager");
 
 		if (e.getEntity().getKiller() instanceof Player) {
 			Player player = e.getEntity().getKiller();
 			EntityType type = e.getEntityType();
-			ifTypeIs(type, EntityType.BLAZE, "blaze", player, blaze);
-			ifTypeIs(type, EntityType.CAVE_SPIDER, "cave_spider", player, cave_spider);
-			ifTypeIs(type, EntityType.CREEPER, "creeper", player, creeper);
-			ifTypeIs(type, EntityType.ENDER_DRAGON, "ender_dragon", player, enderDragon);
-			ifTypeIs(type, EntityType.ENDERMAN, "enderman", player, enderman);
-			ifTypeIs(type, EntityType.ENDERMITE, "endermite", player, endermite);
-			ifTypeIs(type, EntityType.GHAST, "ghast", player, ghast);
-			ifTypeIs(type, EntityType.GIANT, "giant", player, giant);
-			ifTypeIs(type, EntityType.GUARDIAN, "guardian", player, guardian);
-			ifTypeIs(type, EntityType.MAGMA_CUBE, "magma_cube", player, magmaCube);
-			ifTypeIs(type, EntityType.PIG_ZOMBIE, "pig_zombie", player, pigZombie);
-			ifTypeIs(type, EntityType.SILVERFISH, "blaze", player, silverfish);
-			ifTypeIs(type, EntityType.SKELETON, "skeleton", player, skeleton);
-			ifTypeIs(type, EntityType.SLIME, "slime", player, slime);
-			ifTypeIs(type, EntityType.SPIDER, "spider", player, spider);
-			ifTypeIs(type, EntityType.WITCH, "witch", player, witch);
-			ifTypeIs(type, EntityType.WITHER, "wither", player, wither);
-			ifTypeIs(type, EntityType.WITHER_SKELETON, "wither_skeleton", player, witherSkeleton);
-			ifTypeIs(type, EntityType.ZOMBIE, "zombie", player, zombie);
+			ifTypeIs(type, "BLAZE", "blaze", player, blaze);
+			ifTypeIs(type, "CAVE_SPIDER", "cave_spider", player, caveSpider);
+			ifTypeIs(type, "CREEPER", "creeper", player, creeper);
+			ifTypeIs(type, "ELDER_GUARDIAN", "elder_guardian", player, elderGuardian);
+			ifTypeIs(type, "ENDER_DRAGON", "ender_dragon", player, enderDragon);
+			ifTypeIs(type, "ENDERMAN", "enderman", player, enderman);
+			ifTypeIs(type, "ENDERMITE", "endermite", player, endermite);
+			ifTypeIs(type, "EVOKER", "evoker", player, evoker);
+			ifTypeIs(type, "GHAST", "ghast", player, ghast);
+			ifTypeIs(type, "GIANT", "giant", player, giant);
+			ifTypeIs(type, "GUARDIAN", "guardian", player, guardian);
+			ifTypeIs(type, "HUSK", "husk", player, husk);
+			ifTypeIs(type, "ILLUSIONER", "illusioner", player, illusioner);
+			ifTypeIs(type, "MAGMA_CUBE", "magma_cube", player, magmaCube);
+			ifTypeIs(type, "PIG_ZOMBIE", "pig_zombie", player, pigZombie);
+			ifTypeIs(type, "SHULKER", "shulker", player, shulker);
+			ifTypeIs(type, "SILVERFISH", "silverfish", player, silverfish);
+			ifTypeIs(type, "SKELETON", "skeleton", player, skeleton);
+			ifTypeIs(type, "SLIME", "slime", player, slime);
+			ifTypeIs(type, "SPIDER", "spider", player, spider);
+			ifTypeIs(type, "STRAY", "stray", player, stray);
+			ifTypeIs(type, "VEX", "vex", player, vex);
+			ifTypeIs(type, "VINDICATOR", "vindicator", player, vindicator);
+			ifTypeIs(type, "WITCH", "witch", player, witch);
+			ifTypeIs(type, "WITHER", "wither", player, wither);
+			ifTypeIs(type, "WITHER_SKELETON", "wither_skeleton", player, witherSkeleton);
+			ifTypeIs(type, "ZOMBIE", "zombie", player, zombie);
+			ifTypeIs(type, "ZOMBIE_VILLAGER", "zombie_villager", player, zombieVillager);
 		}
 	}
 
