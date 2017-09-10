eade992dc02da428cc53cef4891dbded2bca99e4
Plx0wn <lucas.rouss@gmail.com>
2017-09-10 15:30:13 +0200

    Update Players.java

--- src/fr.plx0wn/Rewards/Players.java
+++ src/fr.plx0wn/Rewards/Players.java
@@ -1,5 +1,6 @@
 package fr.plx0wn.Rewards;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -19,26 +20,49 @@ public class Players implements Listener {
 	@EventHandler
 	public void onEntityKill(EntityDeathEvent e) {
 
-		double playersDeath = config.getDouble("players.death");
-		double playersKill = config.getDouble("players.kill");
-
 		if (e.getEntity().getKiller() instanceof Player) {
-			Player player = e.getEntity().getKiller();
+			Player killed = (Player) e.getEntity();
+			Player killer = e.getEntity().getKiller();
 			EntityType type = e.getEntityType();
+			
+			double playersDeath = config.getDouble("players.death");
+			double playersKill = config.getDouble("players.kill");
+			double all = MobsReward.economy.getBalance(killed);
 
 			if (type.equals(EntityType.PLAYER)) {
 				if (config.getBoolean("enabled.player-death")) {
-					if (player.hasPermission("rewards.players.death") || player.hasPermission("rewards.players.*")) {
-						MobsReward.economy.withdrawPlayer(player.getName(), playersDeath).transactionSuccess();
-						player.sendMessage(Utils.ColoredString(msgconf.getString("players.death")).replace("<loss>",
-								"" + playersDeath));
+					if (killed.hasPermission("rewards.players.death") || killed.hasPermission("rewards.players.*")) {
+						if(config.get("player.death") instanceof String){
+							if(config.getString("player.death").equalsIgnoreCase("all")){
+								MobsReward.economy.withdrawPlayer(killed.getName(), all).transactionSuccess();
+								killed.sendMessage(Utils.ColoredString(msgconf.getString("players.loss.death_all")));
+							} else {
+								Bukkit.getConsoleSender().sendMessage(Utils.ColoredString("&cWrong syntax in config.yml line 23. This line need to be a number or equal to 'all'"));
+							}
+						} else {
+							if (playersDeath < 0) {
+								MobsReward.economy.withdrawPlayer(killed.getName(), -playersDeath).transactionSuccess();
+								killed.sendMessage(Utils.ColoredString(msgconf.getString("players.loss.death"))
+										.replace("<reward>", "" + playersDeath));
+							} else {
+								MobsReward.economy.depositPlayer(killed.getName(), playersDeath).transactionSuccess();
+								killed.sendMessage(Utils.ColoredString(msgconf.getString("players.earn.death"))
+										.replace("<reward>", "" + playersDeath));
+							}
+						}
 					}
 				}
 				if (config.getBoolean("enabled.player-kill")) {
-					if (player.hasPermission("rewards.players.kill") || player.hasPermission("rewards.players.*")) {
-						MobsReward.economy.depositPlayer(player.getName(), playersKill).transactionSuccess();
-						player.sendMessage(Utils.ColoredString(msgconf.getString("players.kill"))
-								.replace("<reward>", "" + playersKill).replace("<player>", e.getEntity().getName()));
+					if (killer.hasPermission("rewards.players.kill") || killer.hasPermission("rewards.players.*")) {
+						if (playersKill < 0) {
+							MobsReward.economy.withdrawPlayer(killer.getName(), -playersKill).transactionSuccess();
+							killer.sendMessage(Utils.ColoredString(msgconf.getString("players.loss.death"))
+									.replace("<reward>", "" + playersKill));
+						} else {
+							MobsReward.economy.depositPlayer(killer.getName(), playersKill).transactionSuccess();
+							killer.sendMessage(Utils.ColoredString(msgconf.getString("players.earn.death"))
+									.replace("<reward>", "" + playersKill));
+						}
 					}
 				}
 			}
